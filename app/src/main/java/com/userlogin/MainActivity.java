1e86984dbb040a07fe34feae4c9c79660831bed9
AnserSkr <49047155+AnserSkr@users.noreply.github.com>
2021-02-07 22:52:56 +0800

    弹出提示框，图片，文字

--- app/src/main/java/com/userlogin/MainActivity.java
+++ app/src/main/java/com/userlogin/MainActivity.java
@@ -3,11 +3,15 @@ package com.userlogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,14 +46,38 @@ public class MainActivity extends AppCompatActivity {
         String Password = edPassword.getText().toString();
         if(Username.equals(DbUser)){
             if (Password.equals(Dbpassword)){
-                Result.setText("登录成功");
+//                Result.setText("登录成功");
+//                Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                LinearLayout layout = (LinearLayout)toast.getView();
+                ImageView img = new ImageView(getApplicationContext());
+                img.setImageResource(R.drawable.wen);
+                layout.addView(img,0);
+                toast.show();
             }else{
-                Result.setText("密码错误，登陆失败");
+//                Result.setText("密码错误，登陆失败");
+//                Toast.makeText(this, "密码错误，登陆失败", Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(this, "密码错误，登陆失败", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                LinearLayout layout = (LinearLayout)toast.getView();
+                ImageView img = new ImageView(getApplicationContext());
+                img.setImageResource(R.drawable.wen);
+                layout.addView(img,0);
+                toast.show();
             }
         }else {
-            Result.setText("用户名不存在，请核对");
+//            Result.setText("用户名不存在，请核对");
+//            Toast.makeText(this, "用户名不存在，请核对", Toast.LENGTH_SHORT).show();
+            Toast toast = Toast.makeText(this, "用户名不存在，请核对", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER,0,0);
+            LinearLayout layout = (LinearLayout)toast.getView();
+            ImageView img = new ImageView(getApplicationContext());
+            img.setImageResource(R.drawable.wen);
+            layout.addView(img,0);
+            toast.show();
         }
-
-
     }
+
+
 }
